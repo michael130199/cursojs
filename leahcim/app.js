ed67a1c025dba0357054fecd914d5f11a7248ee3
Michael U <murbina@consultec-ti.com>
2020-01-30 16:08:44 -0400

    Primer Commit

---
+++ leahcim/app.js
@@ -0,0 +1,26 @@
+var a = "Hola Mundo";
+var b = "hola mundo 10";
+
+var c = a.length;//10
+var d = b.length;//13
+
+var text = ["hola" , " mundo", "ya ", "casi"];
+//var text = [["hola" , " mundo"], ["ya ", "casi"]];
+var text1 = ["Otra", "cosa", "nueva"]; 
+
+if (a > b){
+    console.log("El primero es mayor: " + c);
+} else {
+    console.log("El segundo es mayor: " + d);
+}
+
+
+//.lenght()
+//.push()
+//.pop()
+//.concat()
+
+
+function activar() {
+    console.log(this.text2);
+}
