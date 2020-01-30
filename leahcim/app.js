154c7f4d739ebd723922e852686f5422560d3960
Michael U <murbina@consultec-ti.com>
2020-01-30 16:41:42 -0400

    agregando estilos

--- leahcim/app.js
+++ leahcim/app.js
@@ -5,8 +5,9 @@ var c = a.length;//10
 var d = b.length;//13
 
 var text = ["hola" , " mundo", "ya ", "casi"];
-//var text = [["hola" , " mundo"], ["ya ", "casi"]];
 var text1 = ["Otra", "cosa", "nueva"]; 
+var text2 = [["hola" , " mundo"], ["ya ", "casi"]];
+
 
 if (a > b){
     console.log("El primero es mayor: " + c);
