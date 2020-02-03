10878ce0fa5c99900f2ca386fc2241fcbad42ce5
Abraham Rivas <abraham@arivas.mppeuct.gob.ve>
2020-02-03 16:27:41 -0400

    Agregue Ejercicios con arrays

--- abraham/app.js
+++ abraham/app.js
@@ -2,7 +2,47 @@
 
 var text = "hola mundo";
 
-
 function activar() {
-    document.getElementById('value1').innerHTML=text;
+    
+    
+    let senteces = prompt("Escribe una palabra");
+    let conteoVocales = count(senteces); 
+
+    if (conteoVocales == 0) {
+        document.getElementById('value1').innerHTML="La palabra ingresada tiene  no tiene vocales";
+    } else {
+        document.getElementById('value1').innerHTML="La palabra ingresada tiene "+conteoVocales+ " vocales";
+    }
 };
+
+// ejercicio con arrays and ciclos for para contar bocales de una palabra
+
+function count (senteces) 
+{
+    let arrayV = [];
+
+    for (var i = 0; i < senteces.length; i ++) 
+    {
+        if (senteces[i] == 'a') {
+            arrayV.push(senteces[i]);
+        }
+
+        if (senteces[i] == 'e') {
+            arrayV.push(senteces[i]);
+        }
+
+        if (senteces[i] == 'i') {
+            arrayV.push(senteces[i]);
+        }
+
+        if (senteces[i] == 'o') {
+            arrayV.push(senteces[i]);
+        }
+
+        if (senteces[i] == 'u') {
+            arrayV.push(senteces[i]);
+        }
+    }
+
+    return arrayV.length;
+}
